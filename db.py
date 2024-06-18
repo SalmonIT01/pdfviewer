a29a9bbaa238937fcdad5b9b35109aacb35dfe46
tanuki <panupongnu4@gmail.com>
2024-06-18 14:18:18 +0700

    add pp

--- db.py
+++ db.py
@@ -81,11 +81,12 @@ def get_token(conn,token):
 # print(showdb(conn))
 # print(get_token(conn,'1w2'))
 
-def pee():
-    print('kuy pee')
+# def pee():
+#     print('kuy pee')
 
 
-
+def pp():
+    print('no way action')
 
 
 
